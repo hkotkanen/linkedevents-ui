182d89c04949772e30f5cfda42696f15b0c8f1dc
Henri Kotkanen <henri.kotkanen@gmail.com>
2016-02-12 21:21:41 +0200

    Bare bones image gallery functionality
    -moved image related reducers to own file
    -new components to represent the image grid and single image

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import events from './events.js'
 import user from './user.js'
 import editor from './editor.js'
 import app from './app.js'
+import images from './images.js'
 
 export default {
     events: events,
     user: user,
     editor: editor,
     app: app
+    images: images
 }
